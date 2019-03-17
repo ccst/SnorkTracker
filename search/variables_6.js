729511407cc8663f627b333e66fc43edaffb7b15
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-03-17 09:42:20 +0000

    Deploy Bastelschlumpf/SnorkTracker to github.com/Bastelschlumpf/SnorkTracker.git:gh-pages

---
+++ search/variables_6.js
@@ -0,0 +1,7 @@
+var searchData=
+[
+  ['hastimeout',['hasTimeout',['../class_my_gps.html#a9256bbd73c4358fc7fed3b41cc08de12',1,'MyGps']]],
+  ['hdop',['hdop',['../class_my_gps.html#a0c51f55b750f7621d8addd55c1634ca2',1,'MyGps']]],
+  ['html',['html',['../class_html_tag.html#a678c136cdb1b61c298bc35aba5df1fee',1,'HtmlTag']]],
+  ['humidity',['humidity',['../class_my_data.html#a21be8214f3f4ad946c05db235e6efc79',1,'MyData']]]
+];
